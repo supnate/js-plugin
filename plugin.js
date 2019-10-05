efadce54a7f35b140d3032252284870c6877e6ad
Nate Wang <pwang7@ebay.com>
2019-10-05 20:57:10 +0800

    Initial commit.

---
+++ plugin.js
@@ -0,0 +1,102 @@
+var _plugins = [];
+var _byName = {};
+var _cache = {};
+
+function _isFunc(o) {
+  return !!(o.constructor && o.call && o.apply);
+}
+
+function _get(obj, prop) {
+  var arr = prop.split('.');
+  for (var i = 0; i < arr.length; i++) {
+    if (!obj.hasOwnProperty || !obj.hasOwnProperty(arr[i])) return undefined;
+    obj = obj[arr[i]];
+  }
+  return obj;
+}
+
+function _has(obj, prop) {
+  var arr = prop.split('.');
+  for (var i = 0; i < arr.length; i++) {
+    if (!obj.hasOwnProperty || !obj.hasOwnProperty(arr[i])) return false;
+    obj = obj[arr[i]];
+  }
+  return true;
+}
+
+module.exports = {
+  register: function(p) {
+    if (!p.name) {
+      console.log('Every plugin should have a name.');
+      console.log(p);
+      throw new Error('Every plugin should have a name.');
+    }
+    if (_byName[p.name]) {
+      throw new Error('Plugin "' + p.name + '" already exits.');
+    }
+    _cache = {};
+    _plugins = _plugins.slice();
+    _plugins.push(p);
+    _byName[p.name] = p;
+    if (p.initialize) {
+      p.initialize();
+    }
+  },
+
+  unregister: function(name) {
+    var p = _byName[name];
+    if (!p) throw new Error('Plugin "' + name + '" does\'t exist.');
+    var i = _plugins.indexOf(p);
+    if (i === -1)
+      throw new Error(
+        'Plugin "' +
+          name +
+          '" does\'t exist in _plugins but in _byName. This seems to be a bug of js-plugin.',
+      );
+    _cache = {};
+    _plugins = _plugins.slice();
+    _plugins.splice(i, 1);
+  },
+
+  getPlugin: function(name) {
+    return _byName[name];
+  },
+
+  getPlugins: function(prop) {
+    if (!prop) {
+      return _plugins.filter(p => !p.deps || p.deps.every(dep => !!_byName[dep]));
+    }
+    if (!_cache[prop]) {
+      _cache[prop] = _plugins.filter(p => {
+        if (p.deps && p.deps.some(dep => !_byName[dep])) {
+          // If deps not exist, then not load it.
+          return false;
+        }
+        return _has(p, prop);
+      });
+    }
+    return _cache[prop];
+  },
+
+  invoke: function(prop) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    if (!prop) throw new Error('Invoke on plugin should have prop argument');
+    var noCall = /^!/.test(prop);
+    prop = prop.replace(/^!/, '');
+    var arr = prop.split('.');
+    arr.pop();
+    var obj = arr.join('.');
+    return this.getPlugins(prop).map(function(p) {
+      var method = _get(p, prop);
+      if (!_isFunc(method) || noCall) return method;
+      try {
+        return method.apply(_get(p, obj), args);
+      } catch (err) {
+        // When a plugin failed, doesn't break the app
+        console.log('Failed to invoke plugin: ' + p.name + '!' + prop);
+        console.log(err);
+      }
+      return null;
+    });
+  },
+};
---
+++ test.js
@@ -0,0 +1,64 @@
+const expect = require('chai').expect;
+const plugin = require('./plugin');
+
+const p1 = {
+  name: 'p1',
+  foo: 'bar1',
+  ext: {
+    form: {
+      processMeta(meta) {
+        meta.push('m1');
+      },
+    },
+  },
+};
+
+const p2 = {
+  name: 'p2',
+  foo: 'bar2',
+};
+
+const p3 = {
+  name: 'p3',
+  foo: 'bar3',
+  ext: {
+    form: {
+      processMeta(meta) {
+        meta.push('m3');
+      },
+    },
+  },
+};
+
+plugin.register(p1);
+plugin.register(p2);
+plugin.register(p3);
+
+// getPlugins works
+expect(plugin.getPlugins().length).to.equal(3);
+expect(plugin.getPlugins('ext').length).to.equal(2);
+
+// invoke on functions
+const meta = ['m0'];
+plugin.invoke('ext.form.processMeta', meta);
+expect(meta).to.deep.equal(['m0', 'm1', 'm3']);
+
+// invoke to only collect values
+const bars = plugin.invoke('foo');
+expect(bars).to.deep.equal(['bar1', 'bar2', 'bar3']);
+
+const bars2 = plugin.invoke('!foo');
+expect(bars2).to.deep.equal(['bar1', 'bar2', 'bar3']);
+
+// unregister
+plugin.unregister('p2');
+expect(plugin.getPlugins().map(p => p.name)).to.deep.equal(['p1', 'p3']);
+
+// If deps not exist, don't load it
+plugin.register({ name: 'p4', deps: ['p5'] });
+expect(plugin.getPlugins().map(p => p.name)).to.deep.equal(['p1', 'p3']);
+
+plugin.register({ name: 'p5' });
+expect(plugin.getPlugins().map(p => p.name)).to.deep.equal(['p1', 'p3', 'p4', 'p5']);
+
+console.log('Test success.');
